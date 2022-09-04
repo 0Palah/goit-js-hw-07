a876a90dfe41d486d339cdf9174f40b12d1b3687
Oleg Palashynskyi <palah@ukr.net>
2022-09-04 18:17:10 +0300

    Add gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryListEl = document.querySelector(".gallery");
+
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+  const galleryCard = `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+  return galleryCard;
+};
+
+const makeGalleryArr = galleryItems.map((el) => makeGalleryCard(el));
+// console.log(makeGalleryArr);
+// console.log(galleryListEl);
+galleryListEl.insertAdjacentHTML("beforeend", makeGalleryArr.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
